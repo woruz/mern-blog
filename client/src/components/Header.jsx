1588c4c10ec07e95b2020d05ddcc7ae3b23645d7
sabil <sabil.danish1997@gmail.com>
2024-01-20 16:13:34 +0530

    added sign in flow

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,23 +2,71 @@ import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon, FaSun } from 'react-icons/fa';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { toggleTheme } from '../redux/theme/themeSlice';
+// import { signoutSuccess } from '../redux/user/userSlice';
+import { useEffect, useState } from 'react';
 
-const Header = () => {
+export default function Header() {
   const path = useLocation().pathname;
   const location = useLocation();
   const navigate = useNavigate();
+  // const dispatch = useDispatch();
+  // const { currentUser } = useSelector((state) => state.user);
+  // const { theme } = useSelector((state) => state.theme);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
+  const handleSignout = async () => {
+    // try {
+    //   const res = await fetch('/api/user/signout', {
+    //     method: 'POST',
+    //   });
+    //   const data = await res.json();
+    //   if (!res.ok) {
+    //     console.log(data.message);
+    //   } else {
+    //     dispatch(signoutSuccess());
+    //   }
+    // } catch (error) {
+    //   console.log(error.message);
+    // }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
   return (
     <Navbar className='border-b-2'>
-      <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
-        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Sabil</span>
+      <Link
+        to='/'
+        className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
+      >
+        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+          Sabil's
+        </span>
         Blog
       </Link>
-      <form>
+      <form onSubmit={handleSubmit}>
         <TextInput
           type='text'
           placeholder='Search...'
           rightIcon={AiOutlineSearch}
           className='hidden lg:inline'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
       </form>
       <Button className='w-12 h-10 lg:hidden' color='gray' pill>
@@ -29,9 +77,37 @@ const Header = () => {
           className='w-12 h-10 hidden sm:inline'
           color='gray'
           pill
+          // onClick={() => dispatch(toggleTheme())}
         >
-          
+          {false ? <FaSun /> : <FaMoon />}
         </Button>
+        {false ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt='user' img={currentUser.profilePicture} rounded />
+            }
+          >
+            <Dropdown.Header>
+              <span className='block text-sm'>@{currentUser.username}</span>
+              <span className='block text-sm font-medium truncate'>
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+            <Link to={'/dashboard?tab=profile'}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to='/sign-in'>
+            <Button gradientDuoTone='purpleToBlue' outline>
+              Sign In
+            </Button>
+          </Link>
+        )}
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
@@ -46,7 +122,5 @@ const Header = () => {
         </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
-  )
+  );
 }
-
-export default Header
