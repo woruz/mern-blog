a2da94dc77a7e8927d1581397195f067ba274575
sabil <sabil.danish1997@gmail.com>
2024-01-21 12:40:52 +0530

    add redux toolkit

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,37 +1,42 @@
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux'
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice'
 
 const Signin = () => {
   const [formData, setFormData] = useState({});
+  const {loading,error: errorMessage,currentUser} = useSelector(state => state.user)
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
   const handleSubmit = async (e) => {
-    // e.preventDefault();
-    // if (!formData.email || !formData.password) {
-    //   return dispatch(signInFailure('Please fill all the fields'));
-    // }
-    // try {
-    //   dispatch(signInStart());
-    //   const res = await fetch('/api/auth/signin', {
-    //     method: 'POST',
-    //     headers: { 'Content-Type': 'application/json' },
-    //     body: JSON.stringify(formData),
-    //   });
-    //   const data = await res.json();
-    //   if (data.success === false) {
-    //     dispatch(signInFailure(data.message));
-    //   }
+    e.preventDefault();
+    if (!formData.email || !formData.password) {
+      return dispatch(signInFailure('Please fill all the fields'));
+    }
+    try {
+      dispatch(signInStart());
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
+      }
 
-    //   if (res.ok) {
-    //     dispatch(signInSuccess(data));
-    //     navigate('/');
-    //   }
-    // } catch (error) {
-    //   dispatch(signInFailure(error.message));
-    // }
+      if (res.ok) {
+        dispatch(signInSuccess(data));
+        navigate('/');
+      }
+    } catch (error) {
+      dispatch(signInFailure(error.message));
+    }
   };
   return (
     <div className='min-h-screen mt-20'>
@@ -92,11 +97,11 @@ const Signin = () => {
               Sign Up
             </Link>
           </div>
-          {/* {errorMessage && (
+          {errorMessage && (
             <Alert className='mt-5' color='failure'>
               {errorMessage}
             </Alert>
-          )} */}
+          )}
         </div>
       </div>
     </div>
